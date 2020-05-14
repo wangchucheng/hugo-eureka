f8b674b093948597e1c1d6c8a2f7a88cc89541e8
Wang Chucheng <ccwangchn@gmail.com>
2020-05-14 23:34:42 +0800

    style: change single page style

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  important: true,
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)',
+      }
+    }
   },
   variants: {},
   plugins: []
