69d1aa2b864083627fc6735dea43820fe73dbf80
Wang Chucheng <ccwangchn@gmail.com>
2020-05-27 23:43:36 +0800

    refactor: improve the color scheme

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,5 +1,27 @@
 const themeDir = __dirname + '/../../';
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        themeDir + 'layouts/**/*.html',
+        themeDir + 'exampleSite/content/**/*.html',
+        'layouts/**/*.html',
+        'content/**/*.html',
+    ],
+
+    // This is the function used to extract class names from your templates
+    defaultExtractor: content => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+        return broadMatches.concat(innerMatches)
+    }
+})
+
 module.exports = {    
     plugins: [        
         require('postcss-import')({
@@ -9,5 +31,6 @@ module.exports = {
         require('autoprefixer')({
             path: [themeDir]
         }),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
     ]
 }
--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,16 +1,5 @@
-const themeDir = __dirname + '/../../';
-
 module.exports = {
   important: true,
-  purge: {
-    enabled: true,
-    content: [
-      themeDir + 'layouts/**/*.html',
-      themeDir + 'exampleSite/content/**/*.html',
-      'layouts/**/*.html',
-      'content/**/*.html',
-    ],
-  },
   theme: {
     fontFamily: {
       'serif': ['Lora', 'Noto Serif SC', 'serif'],
