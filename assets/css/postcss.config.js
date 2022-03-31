04c91ff7eb3aed8b98715aa2435ba104182d1b2d
WANG Chucheng <me@wangchucheng.com>
2022-03-31 13:06:08 -0400

    fix: fail to get default style
    Closes #176

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,16 +1,13 @@
-const themeDir = __dirname + '/../../';
+const themeDir = __dirname + "/../../";
 
-module.exports = {    
-    plugins: [        
-        require('postcss-import')({
-            path: [themeDir]
-            }), 
-        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
-        require('autoprefixer')({
-            path: [themeDir]
-        }),
-        require('cssnano')({
-            preset: 'default',
-        }),
-    ]
-}
+module.exports = {
+  plugins: [
+    require("tailwindcss")(themeDir + "assets/css/tailwind.config.js"),
+    require("autoprefixer")({
+      path: [themeDir],
+    }),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
+};
